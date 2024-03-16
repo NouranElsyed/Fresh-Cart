27cbbbd3c941ad5f4cb30ff7e649b6d6cb585c8c
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-03-17 01:16:14 +0200

    modify10

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -5,6 +5,7 @@ import { Category } from 'src/app/interfaces/category';
 import { Product} from 'src/app/interfaces/product';
 import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
+import { WishListService } from 'src/app/services/wish-list.service';
 
 
 @Component({
@@ -13,9 +14,10 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit{
-  constructor(private _ProductService:ProductService,private _Renderer2:Renderer2, private _CartService:CartService, private toastr: ToastrService){}
+  constructor(private _ProductService:ProductService,private _WishListService:WishListService,private _Renderer2:Renderer2, private _CartService:CartService, private toastr: ToastrService){}
   products:Product[] =[];
   categories:Category[] =[];
+  wishListData:string[]=[];
   pageSize:number = 0
   currentPage:number=1
   total:number = 0
@@ -33,9 +35,31 @@ ngOnInit(): void {
       console.log(this.currentPage)
     }
   })
-
+  this._WishListService.getWishList().subscribe({
+    next:(response)=>{
+      const arryOfId= response.data.map((item:any)=> item._id)
+      this.wishListData = arryOfId
+    }})
 }
+addWish(prodId:string):void{
+  this._WishListService.addToWishlist(prodId).subscribe({
+    next:(response)=>{
+      this.toastr.success(response.message)
+      console.log(prodId);
 
+      this.wishListData = response.data
+    }})
+}
+reomveWish(prodId:string):void{
+  this._WishListService.removeFromWishList(prodId).subscribe({
+    next:(response)=>{
+      this.toastr.success(response.message)
+
+      this.wishListData = response.data
+
+
+    }})
+}
 addProduct(id:string,element:HTMLButtonElement):void{
   this._Renderer2.setAttribute(element, 'disabled','true')
   this._CartService.addToCart(id).subscribe({
